0c2ef6fa43e8ef7d6a9d770ae7d43e02261e2ce6
pduchesne <pduchesne@gmail.com>
2019-06-21 14:53:06 +0200

    add EPSG:31370 definition in geojson viewer

--- ckanext/geoview/public/js/geojson_preview.js
+++ ckanext/geoview/public/js/geojson_preview.js
@@ -1,4 +1,8 @@
 // geojson preview module
+
+// add belgian proj definitin
+proj4 && proj4.defs("EPSG:31370", "+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.868628,52.297783,-103.723893,0.336570,-0.456955,1.842183,-1.2747 +units=m +no_defs");
+
 ckan.module('geojsonpreview', function (jQuery, _) {
   return {
     options: {
